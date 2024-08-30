dc426c1cfee0fe3e6c190c39b73270399e5ec865
Srivishal Sivasubramanian <srivishal.sivasubramanian@gmail.com>
2024-08-30 23:03:17 +0530

    feat: title and service block

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,17 +1,10 @@
-import { Link } from 'react-router-dom';
-import logoDark from '../../assets/img/logo-dark.png';
-import logoLight from '../../assets/img/logo-light.png';
+import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-const LogoDark = ({ light }) => {
+const LogoDark = () => {
   return (
-    <Link to='/'>
-      <img
-        src={light ? logoLight : logoDark}
-        alt='AIMass'
-        width='96'
-        height='24'
-      />
+    <Link to="/">
+      <img src="/assets/img/Title.png" alt="AIMass" className="w-36" />
     </Link>
   );
 };
