02e8337faef857bbed22774c6937913548901ac4
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-09-01 02:34:12 +0530

    fix: header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 import Logo from "../logo/Logo";
-import Dashboard from "../../pages/common/Dashboard";
 
 // eslint-disable-next-line react/prop-types
 const Header = ({ loginCSS, signupCSS, navColor, light }) => {
