1be81af34bdee1ad0fb22ff5b1cab8cd869b11f2
kavyansh18 <kavyanshkumar18@gmail.com>
2024-09-01 13:22:32 +0530

    feat:funcupdate

--- src/components/sections/home_01/Content_01.jsx
+++ src/components/sections/home_01/Content_01.jsx
@@ -1,4 +1,6 @@
 import "../../../styles/style.css";
+import { FaArrowRightLong } from "react-icons/fa6";
+
 const Content_01 = () => {
   return (
     <>
@@ -8,7 +10,7 @@ const Content_01 = () => {
         <div className="pb-20 xl:pb-[150px]">
           {/* Section Container */}
           <div className="global-container">
-            <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 lg:gap-20 xl:gap-24 xxl:gap-24">
+            <div className="grid grid-cols-1 items-start justify-start gap-12 md:grid-cols-2 lg:gap-20 xl:gap-24 xxl:gap-12">
               {/* Content Left Block */}
               <div
                 className="jos order-2 overflow-hidden rounded-3xl drop-shadow-2xl md:order-1"
@@ -30,118 +32,105 @@ const Content_01 = () => {
               >
                 {/* Section Content Block */}
                 <div className="flex flex-col items-start justify-start gap-4">
-                  <h2 className=" mb-3 text-[4rem]">Our USPs</h2>
-                  <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
-                    <div class="mx-auto max-w-7xl">
-                      <div class="group relative">
-                        <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                        <div class="items-top relative flex items-center justify-center space-x-6 rounded-lg bg-white px-7 py-6 leading-none ring-1 ring-gray-900/5">
-                          <svg
-                            class="h-8 w-8 text-purple-600"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"
-                            ></path>
-                          </svg>
-                          <div class="space-y-2">
-                            <p class="text-slate-800 text-2xl font-semibold">Digital Signature</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  
-                  <div className="flex flex-col items-start justify-start gap-4">
-                  <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
-                    <div class="mx-auto max-w-7xl">
-                      <div class="group relative">
-                        <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                        <div class="items-top relative flex items-center justify-center space-x-6 rounded-lg bg-white px-7 py-6 leading-none ring-1 ring-gray-900/5">
-                          <svg
-                            class="h-8 w-8 text-purple-600"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"
-                            ></path>
-                          </svg>
-                          <div class="space-y-2">
-                            <p class="text-slate-800 text-2xl font-semibold">Mappls Geo Locations</p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                  <h2 className="mb-3 text-[4rem]">Functanilties</h2>
 
-                <div className="flex flex-col items-start justify-start gap-4">
-                  <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
-                    <div class="mx-auto max-w-7xl">
-                      <div class="group relative">
-                        <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                        <div class="items-top relative flex items-center justify-center space-x-6 rounded-lg bg-white px-7 py-6 leading-none ring-1 ring-gray-900/5">
-                          <svg
-                            class="h-8 w-8 text-purple-600"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"
-                            ></path>
-                          </svg>
-                          <div class="space-y-2">
-                            <p class="text-slate-800 text-2xl font-semibold">Mobile Convenience</p>
+                  <div className="flex w-full items-start justify-start">
+                    <div className="flex items-center justify-center gap-2">
+                      <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
+                        <div class="mx-auto max-w-7xl">
+                          <div class="group relative">
+                            <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-red-500 to-orange-500 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+                            <div class="items-top relative flex w-[21rem] items-center justify-center space-x-6 rounded-lg bg-white px-2 py-5 leading-none ring-1 ring-gray-900/5">
+                              <div class="space-y-2">
+                                <p class="text-2xl font-semibold text-slate-800">
+                                  Digital Signature
+                                </p>
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start justify-start gap-4">
-                  <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
-                    <div class="mx-auto max-w-7xl">
-                      <div class="group relative">
-                        <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-purple-600 to-pink-600 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
-                        <div class="items-top relative flex items-center justify-center space-x-6 rounded-lg bg-white px-7 py-6 leading-none ring-1 ring-gray-900/5">
-                          <svg
-                            class="h-8 w-8 text-purple-600"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              stroke="currentColor"
-                              stroke-linecap="round"
-                              stroke-linejoin="round"
-                              stroke-width="1.5"
-                              d="M6.75 6.75C6.75 5.64543 7.64543 4.75 8.75 4.75H15.25C16.3546 4.75 17.25 5.64543 17.25 6.75V19.25L12 14.75L6.75 19.25V6.75Z"
-                            ></path>
-                          </svg>
-                          <div class="space-y-2">
-                            <p class="text-slate-800 text-2xl font-semibold">Effective Task Prioritization</p>
-                          </div>
-                        </div>
+                      <div>
+                        <p className="mr-2 w-64 text-sm">
+                          Streamline approvals and secure documents with fast,
+                          encrypted digital signatures.
+                        </p>
                       </div>
                     </div>
                   </div>
-                </div>
-                </div>
 
-                
+                  <div className="flex w-full items-start justify-start">
+                    <div className="flex items-center justify-center gap-2">
+                      <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
+                        <div class="mx-auto max-w-7xl">
+                          <div class="group relative">
+                            <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-red-500 to-orange-500 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+                            <div class="items-top relative flex w-[21rem] items-center justify-center space-x-6 rounded-lg bg-white px-2 py-5 leading-none ring-1 ring-gray-900/5">
+                              <div class="space-y-2">
+                                <p class="text-2xl font-semibold text-slate-800">
+                                Mappls Geo Loacations
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <p className="mr-2 w-64 text-sm">
+                        Send location-based alerts for nearby projects or meetings to boost engagement.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex w-full items-start justify-start">
+                    <div className="flex items-center justify-center gap-2">
+                      <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
+                        <div class="mx-auto max-w-7xl">
+                          <div class="group relative">
+                            <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-red-500 to-orange-500 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+                            <div class="items-top relative flex w-[21rem] items-center justify-center space-x-6 rounded-lg bg-white px-2 py-5 leading-none ring-1 ring-gray-900/5">
+                              <div class="space-y-2">
+                                <p class="text-2xl font-semibold text-slate-800">
+                                Effective Task Prioritization
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <p className="mr-2 w-64 text-sm">
+                        Focus on high-priority tasks for better coordination and productivity.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="flex w-full items-start justify-start">
+                    <div className="flex items-center justify-center gap-2">
+                      <div class="relative flex flex-col justify-center overflow-hidden rounded-xl bg-gray-50 sm:p-2">
+                        <div class="mx-auto max-w-7xl">
+                          <div class="group relative">
+                            <div class="absolute -inset-1 rounded-lg bg-gradient-to-r from-red-500 to-orange-500 opacity-25 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
+                            <div class="items-top relative flex w-[21rem] items-center justify-center space-x-6 rounded-lg bg-white px-2 py-5 leading-none ring-1 ring-gray-900/5">
+                              <div class="space-y-2">
+                                <p class="text-2xl font-semibold text-slate-800">
+                                Multilingual
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div>
+                        <p className="mr-2 w-64 text-sm">
+                        Send location-based alerts for nearby projects or meetings to boost engagement.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
               {/* Content Right Block */}
             </div>
