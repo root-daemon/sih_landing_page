e915a3fe85f55565a52b8503f81194ff87231933
Rahuletto <rahulmarban@gmail.com>
2024-09-01 13:31:54 +0530

    feat: header is component now

--- src/pages/common/Board.jsx
+++ src/pages/common/Board.jsx
@@ -1,54 +1,13 @@
-import { useState } from "react";
-import { FaArrowRightLong } from "react-icons/fa6";
-
-import { TbLayoutSidebarRightCollapse } from "react-icons/tb";
-import Notification from "../../components/Notification";
 import BoardComponent from "../../components/Droppable";
-import { useNavigate } from "react-router-dom";
 import Sidebar from "../../components/sidebar/sidebar";
+import PageHeader from "../../components/PageHeader";
 
 export default function Board() {
-  const [notification, setNotification] = useState(false);
-  const [collapse, setCollpase] = useState(true);
-
-  const navigate = useNavigate();
   return (
     <main className="h-screen w-screen overflow-hidden bg-[#D6CFC8]">
-      <header className="flex flex-row items-center justify-between px-12 py-6">
-        <div className="flex flex-row gap-3">
-          <img src="assets/img/icobw.svg" alt="e" className="h-8 w-8" />
-          <h1 className="text-center text-2xl font-semibold uppercase text-black">
-            Coordina
-          </h1>
-        </div>
-        <div className="absolute left-96 top-7 flex items-center justify-center gap-3 text-sm font-medium">
-          <span
-            className="cursor-pointer opacity-50"
-            onClick={() => navigate("/dashboard")}
-          >
-            Departments
-          </span>{" "}
-          <FaArrowRightLong />
-          <span
-            className="cursor-pointer opacity-50"
-            onClick={() => navigate("/projects")}
-          >
-            Tasks
-          </span>{" "}
-          <FaArrowRightLong /> Traffic Management
-        </div>
-        <div
-          onClick={() => setNotification(!notification)}
-          className="cursor-pointer text-3xl text-black"
-        >
-          <TbLayoutSidebarRightCollapse
-            className={`transition-all duration-100 ${notification ? "mr-96" : "rotate-180"}`}
-          />
-        </div>
-      </header>
-      <Notification notification={notification} />
+      <PageHeader />
       <div className="flex h-full w-full flex-row gap-0">
-        <Sidebar collapse={collapse} active={"tasks"} />
+        <Sidebar collapse={true} active={"tasks"} />
         <div className="h-full w-full overflow-auto rounded-tl-3xl bg-gradient-to-br from-[#EFEAE4] to-[#e3d3c7df] p-12 pb-0">
           <h1 className="max-w-[700px] text-4xl font-semibold">
             Maintance Work
