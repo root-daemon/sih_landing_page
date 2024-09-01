7832848e8d6add5eed828e3aab5d06459169848c
Srivishal Sivasubramanian <47695678+root-daemon@users.noreply.github.com>
2024-09-01 10:57:58 +0530

    feat: projects table

--- src/pages/common/Projects.jsx
+++ src/pages/common/Projects.jsx
@@ -16,17 +16,58 @@ import {
   TableHeader,
   TableRow,
 } from "../../components/ui/table";
+import Header from "../../components/Header";
+//s.no
+//description
+//depts (created by)
+// depts (collabs)
+//
 const invoices = [
   {
     title: "Project Kickoff",
     location: "Chennai, Tamil Nadu",
-    Task: "123",
+    description: "A railway project",
+    despartment: "Railway Department",
+    departments: ["Road Department", "Fire Department"],
+    tasks: [],
+  },
+  {
+    title: "Project Kickoff",
+    location: "Chennai, Tamil Nadu",
+    description: "A railway project",
+    despartment: "Railway Department",
+    departments: ["Road Department", "Fire Department"],
+    tasks: [],
+  },
+  {
+    title: "Project Kickoff",
+    location: "Chennai, Tamil Nadu",
+    description: "A railway project",
+    despartment: "Railway Department",
+    departments: ["Road Department", "Fire Department"],
+    tasks: [],
+  },
+  {
+    title: "Project Kickoff",
+    location: "Chennai, Tamil Nadu",
+    description: "A railway project",
+    despartment: "Railway Department",
+    departments: ["Road Department", "Fire Department"],
+    tasks: [],
+  },
+  {
+    title: "Project Kickoff",
+    location: "Chennai, Tamil Nadu",
+    description: "A railway project",
+    despartment: "Railway Department",
+    departments: ["Road Department", "Fire Department"],
+    tasks: [],
   },
 ];
 
 export default function Projects() {
-  const [notification, setNotification] = useState(false);
   const [collapse, setCollpase] = useState(false);
+  const [notification, setNotification] = useState(false);
 
   return (
     <main className="h-screen w-screen overflow-hidden bg-[#D6CFC8]">
@@ -35,7 +76,7 @@ export default function Projects() {
           <Logo />
         </div>
         <div className="absolute left-96 top-7 flex items-center justify-center gap-3 text-sm font-medium">
-          <span className="cursor-pointer">Tasks</span>
+          <span className="cursor-pointer">Board</span>
         </div>
         <div
           onClick={() => setNotification(!notification)}
@@ -50,17 +91,30 @@ export default function Projects() {
       <div className="flex h-full w-full flex-row gap-0">
         <Sidebar collapse={collapse} />
         <div className="h-[92.5vh] w-full overflow-auto rounded-tl-3xl bg-gradient-to-br from-[#EFEAE4] to-[#e3d3c7df] p-12">
-          <h1 className="text-5xl font-semibold">Projects</h1>
+          <h1 className="py-4 text-5xl font-semibold">Projects</h1>
           <Table>
             <TableHeader>
               <TableRow>
-                <TableHead className="w-[100px]">Name</TableHead>
-                <TableHead>Status</TableHead>
-                <TableHead>Method</TableHead>
-                <TableHead className="text-right">Amount</TableHead>
+                <TableHead>S.No</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Location</TableHead>
+                <TableHead>Description</TableHead>
+                <TableHead>Department</TableHead>
+                <TableHead>Collabrators</TableHead>
               </TableRow>
             </TableHeader>
-            <TableBody></TableBody>
+            <TableBody>
+              {invoices.map((project, index) => (
+                <TableRow key={index}>
+                  <TableCell>{index + 1}</TableCell>
+                  <TableCell>{project.title}</TableCell>
+                  <TableCell>{project.location}</TableCell>
+                  <TableCell>{project.description}</TableCell>
+                  <TableCell>{project.despartment}</TableCell>
+                  <TableCell>{project.departments.join(", ")}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
           </Table>
         </div>
       </div>
